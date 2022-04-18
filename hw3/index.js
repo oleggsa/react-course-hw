97220b808ce3f7a041e4d690c0c778994a405939
Braun <braun.ufo@gmail.com>
2022-04-18 23:15:56 +0300

    linted index.js

---
+++ hw3/index.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(sum(10, 20));
+console.log(minus(10, 20));
+console.log(multiply(10, 20));
